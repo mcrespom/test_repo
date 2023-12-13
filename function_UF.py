3f93aecbb54f138e8389f8c20a0e95806ba9f0d3
mcrespom <153688080+mcrespom@users.noreply.github.com>
2023-12-13 18:52:22 -0300

    git

--- function_UF.py
+++ function_UF.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import calendar
 
 class UFCalculator:
     def __init__(self, last_uf_known_date: date, last_uf_value: float, new_ipc: float):
@@ -25,6 +26,31 @@ class UFCalculator:
 
         return uf_values
 
+    def get_ufs(self, date_last_uf: date, last_uf_day_9: float, ipc_day_9: float) -> dict[date, float]:
+        # Validador de la fecha objetivo sea 9
+        if date_last_uf.day != 9:
+            raise ValueError("Error: La función solo admite el día 9 del mes.")
+
+        # Obtener el último día del mes actual
+        ultimo_dia_mes_actual = date_last_uf.replace(day=1) + timedelta(days=calendar.monthrange(date_last_uf.year, date_last_uf.month)[1])
+
+        # Calcular la cantidad total de días en el mes de la última UF conocida
+        dias_en_mes = calendar.monthrange(date_last_uf.year, date_last_uf.month)[1]
+
+        # Calcular la diferencia de días entre el 9 de diciembre y el último día del mes actual
+        diferencia_dias = (ultimo_dia_mes_actual.replace(day=9) - date_last_uf.replace(day=9)).days
+
+        # Inicializar el diccionario de resultados
+        uf_values_result = {}
+
+        # Calcular el valor de la UF para cada día desde la fecha inicial hasta el 9 del siguiente mes
+        for i in range(0, diferencia_dias + 1):
+            fecha_actual = date_last_uf + timedelta(days=i)
+            valor_uf_objetivo = round(last_uf_day_9 * (1 + ipc_day_9) ** (i / dias_en_mes), 2)
+            uf_values_result[fecha_actual] = valor_uf_objetivo
+
+        return uf_values_result
+
 # Ejemplo de uso:
 last_uf_known_date = date(2023, 1, 1)
 last_uf_value = 28000.0
@@ -33,6 +59,10 @@ new_ipc = 0.5
 uf_calculator = UFCalculator(last_uf_known_date, last_uf_value, new_ipc)
 result = uf_calculator.calculate_ufs()
 
-# Imprimir los resultados
+# Imprimir los resultados de la calculadora original
 for date_key, uf_value in result.items():
     print(f"{date_key}: {uf_value}")
+
+# Ejemplo de uso de la nueva función get_ufs
+date_last_uf = date(2023, 1, 9)
+last_uf_day_9 = 28000
--- main_mio.py
+++ main_mio.py
@@ -1,1 +1,31 @@
-from funcion_UF.py import 
+from datetime import date
+from  import UFCalculator
+
+    # Definir parámetros iniciales
+last_uf_known_date = date(2023, 1, 1)
+last_uf_value = 28000.0
+new_ipc = 0.5
+
+    # Crear una instancia de UFCalculator
+uf_calculator = UFCalculator(last_uf_known_date, last_uf_value, new_ipc)
+
+    # Calcular UF con la función calculate_ufs
+result_calculate_ufs = uf_calculator.calculate_ufs()
+
+    # Imprimir los resultados de la función calculate_ufs
+print("Resultados de calculate_ufs:")
+for date_key, uf_value in result_calculate_ufs.items():
+    print(f"{date_key}: {uf_value}")
+
+    # Definir parámetros para la función get_ufs
+date_last_uf = date(2023, 1, 9)
+last_uf_day_9 = 28000
+ipc_day_9 = 0.5
+
+    # Calcular UF con la función get_ufs
+result_get_ufs = uf_calculator.get_ufs(date_last_uf, last_uf_day_9, ipc_day_9)
+
+    # Imprimir los resultados de la función get_ufs
+print("\nResultados de get_ufs:")
+for date_key, uf_value in result_get_ufs.items():
+    print(f"{date_key}: {uf_value}")
